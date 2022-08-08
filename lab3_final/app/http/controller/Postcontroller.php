03c26a7c725dd5ecc29e609ead775d068df58abc
AyaIbrahim3027 <102617431+AyaIbrahim3027@users.noreply.github.com>
2022-08-08 23:11:00 +0200

    Create Postcontroller.php

---
+++ lab3_final/app/http/controller/Postcontroller.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts= Post::all();
+        //dd($users);
+        return view ('posts.index')->with(['posts' => $posts]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('posts.create') ;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->all();
+        $post = new Post([
+            'title' => $request->get('title'),
+            'body'=> $request->get('body'),
+            'enabled'=> $request->get('enabled')
+        ]);
+        $post->save();
+        return 'success post has been added';
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $post = Post::find($id);
+
+        return view ('posts.show')->with(['posts' => $post]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $post=Post::find($id);  
+        return view('Post.edit')->with(['post' => $post]) ;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request->only('title','body','enabled');
+        Post::where('id', $id)->update([
+        'title' =>$request['title'],
+        'body' =>$request['body'],
+        'enabled' =>$request['enabled'],
+        ]);
+        return 'success post has been updated';
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Post::where('id', $id)->delete();
+        return 'success post has been deleted';
+
+    }
+
+    public function trashed()
+    {
+        $post = Post::onlyTrashed()->get();
+       return view('posts.softdelete')->with('posts',$post);
+    }
+
+
+    public function restore($id)
+    {
+        $post = Post::withTrashed()->where('id',$id)->first();
+        $post->restore();
+        return redirect()->route('posts.index');
+    }
+
+}
